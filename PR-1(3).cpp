e91c96ad0f9e3fc7b96cd9e618eb0a09630b3eec
pri-245 <geetaghinaiya01@gmail.com>
2025-05-09 13:54:59 +0530

    Add files via upload

---
+++ PR-1(3).cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    float a,b,c,d;
+	
+	printf("enter value of a:");
+	scanf("%f",&a);  
+	
+	printf("enter value of b:");
+	scanf("%f",&b);  
+	
+    c=180-(a+b);
+	
+	printf("c is :%f",c);
+}
