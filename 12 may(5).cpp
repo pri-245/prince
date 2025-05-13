df9b41f2bda88151dbaa1f0de3557dfe577b3db7
pri-245 <geetaghinaiya01@gmail.com>
2025-05-13 16:02:44 +0530

    Add files via upload

---
+++ 12 may(5).cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int first_number,second_number,c;
+	second_number;
+	
+	printf(" enter first number:");
+	scanf("%d",&first_number);
+	
+	printf(" enter second number:");
+	scanf("%d",&second_number);
+	
+	while(first_number<=second_number)
+	
+	{
+		printf("%d\n",first_number);
+	first_number+=4;
+		
+	}
+	
+	
+}
