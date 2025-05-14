4e7c0781e1261f3ce3934e96526979e798c7c647
pri-245 <geetaghinaiya01@gmail.com>
2025-05-14 18:21:53 +0530

    Add files via upload

---
+++ 14 may(1).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=1,b=2,sum=0,N;
+	
+	printf("Enter value of N :");
+	scanf("%d",&N);
+	
+	for(int i=1;i<=N;i++)
+	{
+		sum +=i;
+		
+	
+	}
+	printf("sum of all number :%d",sum);
+	
+}
---
+++ 14 may(2).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=1,b=2,multi=1,N;
+	
+	printf("Enter value of N :");
+	scanf("%d",&N);
+	
+	for(int i=1;i<=N;i++)
+	{
+		multi *=i;
+		
+	
+	}
+	printf("miltification of all number :%d",multi);
+	
+}
